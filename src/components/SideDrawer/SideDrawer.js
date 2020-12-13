aba98d0c4087b2e0aea9de8f00d7a4feb808be49
Emiel de Jong <emieldejong@xs4all.nl>
2020-12-13 14:03:05 +0100

    close button

--- src/components/SideDrawer/SideDrawer.js
+++ src/components/SideDrawer/SideDrawer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ReactComponent as CloseIcon } from '../../assets/close-icon.svg';
 import './SideDrawer.css';
 
 function SideDrawer({ show }) {
@@ -11,6 +12,7 @@ function SideDrawer({ show }) {
 
     return (
         <nav className={drawerClasses}>
+            <button type="button"><CloseIcon /></button>
             <ul>
                 <li><a href="/">Products</a></li>
                 <li><a href="/">Users</a></li>
