36e674433b16cfc16ac25c3462129cb30c00d5f2
Emiel de Jong <emieldejong@xs4all.nl>
2020-12-22 13:54:01 +0100

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as Logo } from '../assets/logo.svg';
 import './Home.css';
 
 function Home() {
     return (
-        <div className="big-logo">
-            {/* <Logo style={{ maxWidth: 600 }} title="dropit" /> */}
+        // <div style={{ height: '100%' }}>
+        <div className="container-center">
+            <h2>"Now Give Me A Beat!"</h2>
+            <div className="action-btns">
+                <NavLink type="button" to="/sign-in" >Sign In</NavLink>
+                <NavLink className="border" type="button" to="/sign-up" >Sign Up</NavLink>
+            </div>
+            <ul>
+                <li>Send your demo</li>
+                <li>Get feedback from Don Diablo</li>
+                <li>Change the history of music...</li>
+            </ul>
         </div>
+        // </div>
     )
 }
 
