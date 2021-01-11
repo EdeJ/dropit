60d688a4f5a18c260bc91b28421c95de4e8ccd1d
Emiel de Jong <emieldejong@xs4all.nl>
2021-01-11 14:03:35 +0100

    update

--- src/pages/AddDemo.js
+++ src/pages/AddDemo.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 function AddDemo() {
+
+
     return (
         <div className="full-page">
             <h3>Add new demo</h3>
+            <form action="localhost:8080/files" method="post">
+                <input type="file" id="myFile" name="filename" />
+                <input type="submit" />
+            </form>
         </div>
+
     )
 }
 
