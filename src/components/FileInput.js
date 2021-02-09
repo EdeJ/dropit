36f26a9585bcdb1d9d56f0615fdcb97bdee12790
Emiel de Jong <emieldejong@xs4all.nl>
2021-02-09 16:28:14 +0100

    update

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import { useFormContext } from 'react-hook-form'
+// import React from 'react'
+// import { useFormContext } from 'react-hook-form'
+// import styles from './TextInput.module.css'
 
-function FileInput({ name, label, type, fieldRef }) {
+// function FileInput({ name, label, type, fieldRef }) {
 
-    const { errors } = useFormContext()
+//     const { errors } = useFormContext()
 
-    return (
-        <div className="input-section">
-            <label htmlFor={name}>{label}</label>
-            <input
-                className={errors[name] ? 'error' : ''}
-                type={type}
-                id={name}
-                name={name}
-                ref={fieldRef}
-            />
-            {errors[name] &&
-                <p className="error-message">
-                    {errors[name].message}
-                </p>
-            }
-        </div>
-    )
-}
+//     return (
+//         <div className={styles['input-section']}>
+//             <label htmlFor={name}>{label}</label>
+//             <input
+//                 className={errors[name] ? styles['error'] : ''}
+//                 type={type}
+//                 id={name}
+//                 name={name}
+//                 ref={fieldRef}
+//             />
+//             {errors[name] &&
+//                 <p className={styles['error-message']}>
+//                     {errors[name].message}
+//                 </p>
+//             }
+//         </div>
+//     )
+// }
 
-export default FileInput
+// export default FileInput
--- src/pages/myDemos/AddDemo.js
+++ src/pages/myDemos/AddDemo.js
@@ -19,7 +19,6 @@ function AddDemo() {
     const history = useHistory()
 
     async function onSubmit(data) {
-        setMessage()
 
         const formData = new FormData()
         formData.append('file', data.file[0])
@@ -29,13 +28,12 @@ function AddDemo() {
         formData.append('artist', data.artist)
 
         try {
-            const result = await axiosConfig.post('api/files', formData, {
+            await axiosConfig.post('api/files', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                     'Authorization': user.accessToken
                 }
             })
-            setMessage({ text: result.data, type: 'success' })
             history.push('/my-demos')
         } catch (error) {
             console.log(error)
@@ -44,56 +42,56 @@ function AddDemo() {
     }
 
     return (
-        <div className="full-page">
-            <h3>Add new demo</h3>
-            <FormProvider errors={errors} >
-                <form className="dropit-form" onSubmit={handleSubmit(onSubmit)}>
-                    {/* // TODO form className   */}
-                    <FileInput
-                        type="file"
-                        name="file"
-                        fieldRef={register({
-                            required: {
-                                value: true,
-                                message: "You must add a mp3 audio file"
+        <div className="page">
+            <div className="content">
+                <h3>Add new demo</h3>
+                <FormProvider errors={errors} >
+                    <form className="dropit-form" onSubmit={handleSubmit(onSubmit)}>
+                        <TextInput
+                            type="file"
+                            name="file"
+                            fieldRef={register({
+                                required: {
+                                    value: true,
+                                    message: "You must add a mp3 audio file"
+                                }
+                            })
                             }
-                        })
-                        }
-                    />
-                    <TextInput
-                        type="text"
-                        label="Song title"
-                        name="songTitle"
-                        fieldRef={register({
-                            required: {
-                                value: true,
-                                message: "You must specify a song title"
+                        />
+                        <TextInput
+                            type="text"
+                            label="Song title"
+                            name="songTitle"
+                            fieldRef={register({
+                                required: {
+                                    value: true,
+                                    message: "You must specify a song title"
+                                }
+                            })
                             }
-                        })
-                        }
-                    />
-                    <TextInput
-                        type="text"
-                        label="Artist"
-                        name="artist"
-                        fieldRef={register({
-                            required: {
-                                value: true,
-                                message: "You must specify an artist name"
+                        />
+                        <TextInput
+                            type="text"
+                            label="Artist"
+                            name="artist"
+                            fieldRef={register({
+                                required: {
+                                    value: true,
+                                    message: "You must specify an artist name"
+                                }
+                            })
                             }
-                        })
-                        }
-                    />
-                    <button>upload</button>
-                </form>
-            </FormProvider>
-            {message && (
-                <FlashMessage duration={5000}>
-                    <strong>{message.text}</strong>
-                </FlashMessage>
-            )}
-        </div >
-
+                        />
+                        <button>upload</button>
+                    </form>
+                </FormProvider>
+                {message && (
+                    <FlashMessage duration={5000}>
+                        <strong>{message.text}</strong>
+                    </FlashMessage>
+                )}
+            </div>
+        </div>
     )
 }
 
